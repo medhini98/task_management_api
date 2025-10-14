f05235e7932ba692f4895eaedb42de4a5dea4d3a
Medhini Sridharr <medhini.sridharr@gmail.com>
2025-10-14 13:58:15 +0530

    feat: update root endpoint

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI() #creating instance of fastapi
 
 @app.get("/") #define route using this decorator - tells FastAPI that func root handles GET requests to root URL ("/")
 async def root():
-    return {"message":"Hello, FastAPI!"} #returns JSON object
+    return {"message":"Welcome to the Todo App!"} #returns JSON object
